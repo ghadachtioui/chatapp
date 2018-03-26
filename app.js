4b0a257c3355a2264edc30fae063729dfa897783
ghadachtioui <37656236+ghadachtioui@users.noreply.github.com>
2018-03-26 11:59:42 +0100

    Update app.js

--- app.js
+++ app.js
@@ -11,21 +11,7 @@ restService.use(
   })
 );
 
-restService.use(bodyParser.json());
 
-restService.post("/echo", function(req, res) {
-  var speech =
-    req.body.result &&
-    req.body.result.parameters &&
-    req.body.result.parameters.echoText
-      ? req.body.result.parameters.echoText
-      : "Seems like some problem. Speak again.";
-  return res.json({
-    speech: speech,
-    displayText: speech,
-    source: "webhook-echo-sample"
-  });
-});
 //
 // Preamble
 var http = require ('http');	     // For serving a basic web page.
