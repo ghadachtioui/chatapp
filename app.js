30da06a4cf488dc00dc9d5af758fdd0fd97c3498
ghadachtioui <37656236+ghadachtioui@users.noreply.github.com>
2018-03-26 15:05:56 +0100

    Update app.js

--- app.js
+++ app.js
@@ -15,9 +15,8 @@ restService.use(bodyParser.json());
 restService.post("/echo", function(req, res) {
   var speech =
     req.body.result &&
-    req.body.result.parameters &&
-    req.body.result.parameters.echoText
-      ? req.body.result.parameters.echoText
+    req.body.result.resolvedQuery &&
+      ? req.body.result.resolvedQuery
       : "Seems like some problem. Speak again.";
   return res.json({
     speech: speech,
