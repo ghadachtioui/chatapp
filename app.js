6d97c99451903e76370e228ca9201dc81986c498
ghadachtioui <37656236+ghadachtioui@users.noreply.github.com>
2018-03-26 14:24:47 +0100

    Update app.js

--- app.js
+++ app.js
@@ -15,9 +15,8 @@ restService.use(bodyParser.json());
 restService.post("/echo", function(req, res) {
   var speech =
     req.body.result &&
-    req.body.result.parameters &&
-    req.body.result.parameters.echoText
-      ? req.body.result.parameters.echoText
+    req.body.result.resolvedQuery
+      ? req.body.result.resolvedQuery
       : "Seems like some problem. Speak again.";
   return res.json({
     speech: speech,
@@ -41,7 +40,7 @@ restService.post("/audio", function(req, res) {
     displayText: "speech",
     source: "webhook-echo-sample",
     data: {
-      slack: slack_message
+      speech
     }
   });
 //
