d3657542e484c193aaba664a66c5ad21f0dbd806
ghadachtioui <37656236+ghadachtioui@users.noreply.github.com>
2018-03-27 09:44:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,20 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-
 const restService = express();
+
+var http = require ('http');	     // For serving a basic web page.
+var mongoose = require ("mongoose"); // The reason for this demo.
+
+// Here we find an appropriate database to connect to, defaulting to
+// localhost if we don't find one.  
+var uristring = 
+  process.env.MONGODB_URI || 
+  'mongodb://jazz:jazzo@ds115799.mlab.com:15799/mydb';
+
+// The http server will listen to an appropriate port, or default to
+// port 5000.
+var theport = process.env.PORT || 5000;
 
 restService.use(
   bodyParser.urlencoded({
@@ -26,20 +38,7 @@ var speech =
     });
 });
 
-//
-// Preamble
-var http = require ('http');	     // For serving a basic web page.
-var mongoose = require ("mongoose"); // The reason for this demo.
 
-// Here we find an appropriate database to connect to, defaulting to
-// localhost if we don't find one.  
-var uristring = 
-  process.env.MONGODB_URI || 
-  'mongodb://jazz:jazzo@ds115799.mlab.com:15799/mydb';
-
-// The http server will listen to an appropriate port, or default to
-// port 5000.
-var theport = process.env.PORT || 5000;
 
 // Makes connection asynchronously.  Mongoose will queue up database
 // operations and release them when the connection is complete.
@@ -61,8 +60,6 @@ var userSchema = new mongoose.Schema({
   age: { type: Number, min: 0}
 });
 
-// Compiles the schema into a model, opening (or creating, if
-// nonexistent) the 'PowerUsers' collection in the MongoDB database
 var PUser = mongoose.model('PowerUsers', userSchema);
 
 // Clear out old data
@@ -81,23 +78,6 @@ var johndoe = new PUser ({
 // Saving it to the database.  
 johndoe.save(function (err) {if (err) console.log ('Error on save!')});
 
-// Creating more users manually
-var janedoe = new PUser ({
-  name: { first: 'Jane', last: 'Doe' },
-  age: 65
-});
-janedoe.save(function (err) {if (err) console.log ('Error on save!')});
-
-// Creating more users manually
-var alicesmith = new PUser ({
-  name: { first: 'Alice', last: 'Smith' },
-  age: 45
-});
-alicesmith.save(function (err) {if (err) console.log ('Error on save!')});
-
-
-// In case the browser connects before the database is connected, the
-// user will see this message.
 var found = ['DB Connection not yet established.  Try again later.  Check the console output for error messages if this persists.'];
 
 restService.listen(process.env.PORT || 8000, function() {
