f3d534e45b248ce4fe79a7f7b0d299fa1798b202
ghadachtioui <37656236+ghadachtioui@users.noreply.github.com>
2018-03-27 11:29:06 +0100

    Update app.js

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ var speech =
 
 // Makes connection asynchronously.  Mongoose will queue up database
 // operations and release them when the connection is complete.
-mongoose.connect(uristring, function (err, res) {
+MongoClient.connect(uristring, function (err, res) {
   if (err) { 
     console.log ('ERROR connecting to: ' + uristring + '. ' + err);
   } else {
